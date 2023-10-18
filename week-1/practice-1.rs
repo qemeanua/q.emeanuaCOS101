5a275d62ca0b82f77023aa677ceb39eecb089a8c
q.emeanuaCOS101 <queen-esther.emeanua@pau.edu.ng>
2023-10-18 23:58:28 +0100

    (My first commit)

---
+++ week-1/practice-1.rs
@@ -0,0 +1,14 @@
+fn main(){
+	println!("Welcome to COS 101");
+
+	println!("The course learning outcome is as follows:")
+
+	println!("1. Distuingish between computer and computer programming,/n
+	          2. Develop some techniques i computer sciences,/n
+	          3. Unnderstand the dfferent areas of study in computer science,/n 
+	          4. Conversant wth applcantionns of computer science,/n 
+	          5. Navigate the career prospects in computer science./n 
+	          6. Conversant with computer programming concepts." )
+
+	   
+}
